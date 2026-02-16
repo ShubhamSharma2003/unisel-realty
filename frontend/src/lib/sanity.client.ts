5579ef259c8269b70e0dcd68abe3cd39e18d4c6d
Shubham Sharma <shubham.sharma@nexxbase.com>
2026-02-16 16:31:57 +0530

    chnages

--- frontend/src/lib/sanity.client.ts
+++ frontend/src/lib/sanity.client.ts
@@ -1,9 +1,8 @@
 import { createClient } from "next-sanity";
 
-const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "uqxf8913";
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-10-01";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
 
 export const sanityConfig = {
   projectId,
